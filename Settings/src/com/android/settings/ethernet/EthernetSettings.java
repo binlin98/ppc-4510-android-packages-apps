d57ff76f4e2456ec3b4149e5f7cfeea26fb4f9ef
wuwenbing <wuwenbing@norco.com.cn>
2017-07-15 09:36:14 +0800

    add ethernet MAC show

--- Settings/src/com/android/settings/ethernet/EthernetSettings.java
+++ Settings/src/com/android/settings/ethernet/EthernetSettings.java
@@ -47,6 +47,8 @@ import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.content.Intent;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 import java.util.regex.Pattern;
 import java.lang.Integer;
@@ -166,7 +168,7 @@ public class EthernetSettings extends SettingsPreferenceFragment
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.ethernet_settings);
-        
+
         mEthManager = (EthernetManager) getSystemService(Context.ETHERNET_SERVICE);
         
         if (mEthManager == null) {
@@ -236,8 +238,9 @@ public class EthernetSettings extends SettingsPreferenceFragment
     }
     private void refreshUI ( ) {
   
-    //    setStringSummary(KEY_ETH_HW_ADDRESS,mEthHwAddress);
-  
+	mEthHwAddress = getLocalMacAddressFromBusybox();
+
+        setStringSummary(KEY_ETH_HW_ADDRESS,mEthHwAddress);
         setStringSummary(KEY_ETH_IP_ADDRESS, mEthIpAddress);
         setStringSummary(KEY_ETH_NET_MASK, mEthNetmask);
         setStringSummary(KEY_ETH_GATEWAY, mEthGateway);
@@ -469,37 +472,80 @@ public class EthernetSettings extends SettingsPreferenceFragment
 		mEthdns2=dnsServers.get(1).getHostAddress();
 	}		
    }
-	/*
-	 * TODO:
-	 */
-    public void getEthInfo(){
-		/*
-		mEthHwAddress = mEthManager.getEthernetHwaddr(mEthManager.getEthernetIfaceName());
-		if (mEthHwAddress == null) mEthHwAddress = nullIpInfo;
-		*/
-        IpAssignment mode =mEthManager.getConfiguration().getIpAssignment();
-        
-	   
-         if ( mode== IpAssignment.DHCP) {
-	   /*
-	    * getEth from dhcp
-	   */
-            getEthInfoFromDhcp();
-	} else if(mode == IpAssignment.STATIC) {
-	   /*
-	    * TODO: get static IP
-	   */
-            getEthInfoFromStaticIp();
-	}     
+
+    public static String getLocalMacAddressFromBusybox(){   
+	    String result = "";     
+	    String Mac = "";
+	    result = callCmd("busybox ifconfig eth0","HWaddr");
+
+	    //如果返回的result == null，则说明网络不可取
+	    if(result==null){
+		    return "网络出错，请检查网络";
+	    }
+
+	    //对该行数据进行解析
+	    //例如：eth0      Link encap:Ethernet  HWaddr 00:16:E8:3E:DF:67
+	    if(result.length()>0 && result.contains("HWaddr")==true){
+		    Mac = result.substring(result.indexOf("HWaddr")+6, result.length()-1);
+		    result = Mac;          
+	    }
+	    return result;
+    }  
+
+    private static String callCmd(String cmd,String filter) {   
+	    String result = "";   
+	    String line = "";   
+	    try {
+		    Process proc = Runtime.getRuntime().exec(cmd);
+		    InputStreamReader is = new InputStreamReader(proc.getInputStream());   
+		    BufferedReader br = new BufferedReader (is);   
+
+		    //执行命令cmd，只取结果中含有filter的这一行
+		    while ((line = br.readLine ()) != null && line.contains(filter)== false) {   
+			    //result += line;
+			    //  Log.i("test","line: "+line);
+		    }   
+		    result = line;
+	    }   
+	    catch(Exception e) {   
+		    e.printStackTrace();   
+	    }   
+	    return result;   
     }
+    /*
+     * TODO:
+     */
+    public void getEthInfo(){
+
+
+
+	    if (mEthHwAddress == null) mEthHwAddress = nullIpInfo;
+
+	    IpAssignment mode =mEthManager.getConfiguration().getIpAssignment();
+
+
+	    if ( mode== IpAssignment.DHCP) {
+		    /*
+		     * getEth from dhcp
+		     */
+		    getEthInfoFromDhcp();
+	    } else if(mode == IpAssignment.STATIC) {
+		    /*
+		     * TODO: get static IP
+		     */
+		    getEthInfoFromStaticIp();
+	    }     
+    }
+
+
 
     /*
      * tools
-    */
+     */
     private void log(String s) {
-        Log.d(TAG, s);
+	    Log.d(TAG, s);
     }
-    
+
     @Override
     public void onClick(DialogInterface dialogInterface, int button) {
     	if(button==ethernet_static_ip_dialog.BUTTON_SUBMIT) {
